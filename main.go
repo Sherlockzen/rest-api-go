0c02b3d9c7afacc0b91f6fe77d7a81b4955c6f8d
Hewerton <hewertonlourenco@gmail.com>
2023-03-31 22:35:11 -0300

    Feat: using the data.json as fake DB

--- main.go
+++ main.go
@@ -1,41 +1,62 @@
 package main
 
 import (
-
-  "net/http"
-  "github.com/gin-gonic/gin"
-  "os"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
-
 
 // album represents data about a record album.
 type album struct {
-  ID      string  `json:"id"`
-  Title   string  `json:"title"`
-  Artist  string  `json:"artist"`
-  Price   float64 `json:"price"`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 // albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+// var albums = []album{
+// 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+// 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+// 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+// }
 
-func main()  {
-  router := gin.Default()
-  router.GET("/albums", getAlbums)
+func main() {
+	router := gin.Default()
 
-  // router.Run("localhost:8080")
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "3000"
-  }
-  router.Run("0.0.0.0:" + port)
+ //  albums := []album{}
+
+	// content, er := ioutil.ReadFile("dat.json")
+	// if er != nil {
+	// 	log.Fatal(er)
+	// }
+
+	// er = json.Unmarshal(content, &albums)
+
+	router.GET("/albums", getAlbums)
+
+	// router.Run("localhost:8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	router.Run("0.0.0.0:" + port)
 }
 
 // getAlbums responds with the list of all albums as JSON
-func getAlbums(c *gin.Context)  {
-  c.IndentedJSON(http.StatusOK, albums)
+func getAlbums(c *gin.Context) {
+
+  albums := []album{}
+
+	content, er := ioutil.ReadFile("data.json")
+	if er != nil {
+		log.Fatal(er)
+	}
+
+	er = json.Unmarshal(content, &albums)
+
+	c.IndentedJSON(http.StatusOK, albums)
 }
